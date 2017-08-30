680ec33814b463b5f6882a142064e9531b0f9bf5
Dave <ghasimi.finance@gmail.com>
2017-08-30 18:23:55 -0400

    First version

---
+++ example.m
@@ -0,0 +1,14 @@
+% An example for hist2() function
+% August 2017
+
+rng(2)
+z = randn(1000,1);
+
+figure(1)
+hist2(z,-2,2)
+title('hist2(z,-2,2) Example')
+
+figure(2)
+x = z*.2 + exp(z)*.4;
+hist2(x,0,3)
+title('hist2(x,-1,3) Example')
---
+++ hist2.m
@@ -0,0 +1,176 @@
+function [h p1 p2 p3] = hist2(x, a, b)
+% This function is a histogram plus the cummulative distributions between
+% critical points a and b.
+%   [h p1 p2 p3] = hist2(x, a, b)
+%       h : Handle to the plot
+%       p1: Percentage of observations which are <= a
+%       p2: Percentage of observations between a and b
+%       p2: Percentage of observations which are >= b
+%   
+%   hist2 gets the vector x and one or two critical points (a and b),
+%   and generates a histogram, plus the probabilities for observations 
+%   between and outside of the critical points. 
+%
+%   Example: 
+%       x = randn(1000,1);
+%       [h p1 p2 p3] = hist2(x, -1.67, 2.33)
+
+% Handling of inputs ----------------------------------------------
+if (nargin == 1)
+    a = median(x);
+    b = max(x); 
+elseif (nargin == 2)
+    b = max(x);    
+end
+
+% 
+if (size(x,2)>1)
+    x = x';
+end
+
+% 
+if (b<a) 
+    c = a; 
+    a = b;
+    b = c;
+end
+
+% Parameters for visualization ------------------------------
+
+% CDFs
+n = length(x);
+pL = sum(x<=a)/n*100;
+pR = 100 - sum(x<=b)/n*100;
+
+% Color and Formats
+MC1  = 'w';
+MC2  = 'k';
+C1 = [0 .6 1];
+C2 = [1 1 1];
+C3 = [1 1 1];
+fAlpha = 0.6;
+e = 0.065;
+
+% Histogram and numerical outputs ---------------------------------
+
+h = histogram(x);
+p1= pL/100;
+p3= pR/100;
+p2= 1 - p1 - p3;
+
+% Annotations ----------------------------------------------------
+
+% Parameters
+pa = gca;
+pa.YLim = [pa.YLim(1), pa.YLim(2)*1.12];
+
+p = pa.Position;
+sf = p(4)-p(2);
+W = abs(pa.XLim(2) - pa.XLim(1));
+
+L = abs(a-pa.XLim(1));
+R = abs(b-pa.XLim(1));
+
+oh = .06 * sf;
+oy = p(4) + p(2) - 2.2*oh;
+
+%
+line([a a],[0 pa.YLim(2)*.95],'Color','k','LineStyle','--')
+line([b b],[0 pa.YLim(2)*.95],'Color','k','LineStyle','--')
+
+
+% L - Left-side cummulative distribution
+ox = p(1);
+ow = (L/W)*p(3);
+
+dim = [ox p(2) ow oy-p(2)+oh];
+an = annotation('rectangle',dim);
+an.FaceColor = C3;
+an.EdgeColor = 'none';
+an.FaceAlpha = fAlpha;
+
+dim = [ox oy ow oh];
+an = annotation('textbox',dim,'String',string(num2str(pL,'%.1f%%')));
+if(ow < e) an.String=''; end
+an.BackgroundColor = 'none';
+an.EdgeColor = 'k';
+an.Color = MC2;
+an.HorizontalAlignment = 'center';
+an.VerticalAlignment = 'middle';
+
+% R - Right-side cummulative distribution
+ox = p(1)+(R/W)*p(3);
+ow = ((W-R)/W)*p(3);
+
+dim = [ox p(2) ow oy-p(2)+oh];
+an = annotation('rectangle',dim);
+an.FaceColor = C3;
+an.EdgeColor = 'none';
+an.FaceAlpha = fAlpha;
+
+dim = [ox oy ow oh];
+an = annotation('textbox',dim,'String',string(num2str(pR,'%.1f%%')));
+if(ow < e) 
+    an.String=''; 
+end
+an.BackgroundColor = 'none';
+an.EdgeColor = 'k';
+an.Color = MC2;
+an.HorizontalAlignment = 'center';
+an.VerticalAlignment = 'middle';
+
+% M - Middle cummulative distribution
+ox = p(1) + (L/W)*p(3);
+ow = ((R-L)/W)*p(3);
+dim = [ox oy ow oh];
+an = annotation('textbox',dim,'String',string(num2str(100-pL-pR,'%.1f%%')));
+if(ow < e) 
+    an.String=''; 
+end
+an.BackgroundColor = C1;
+an.EdgeColor = C1;
+an.Color = MC1;
+if(ow < e) 
+    an.String=''; 
+end
+an.HorizontalAlignment = 'center';
+an.VerticalAlignment = 'middle';
+pa.YLim(2) = pa.YLim(2)*1.05;
+
+% Tick labels for the cummulative distribution --------------------
+oy = p(4) + p(2) - 1.2*oh;
+
+% Lower point
+ox = p(1) + (L/W)*p(3) - 0.02;
+ow = 0.2;
+dim = [ox oy ow oh];
+an = annotation('textbox',dim,'String',string(num2str(a)),'FitBoxToText','on');
+if(ow < e) 
+    an.String=''; 
+end
+an.EdgeColor = 'none';
+an.Color = 'k';
+if(ow < e) 
+    an.String=''; 
+end
+an.HorizontalAlignment = 'left';
+an.VerticalAlignment = 'middle';
+
+% Upper point
+ox = p(1) + (R/W)*p(3)  - 0.025;
+ow = 0.2;
+dim = [ox oy ow oh];
+an = annotation('textbox',dim,'String',string(num2str(b)),'FitBoxToText','on');
+if(ow < e) 
+    an.String=''; 
+end
+an.EdgeColor = 'none';
+an.Color = 'k';
+if(ow < e) 
+    an.String=''; 
+end
+an.HorizontalAlignment = 'left';
+an.VerticalAlignment = 'middle';
+
+end
+
